3dcf1ce673ebd9c7d024fb45339ec1a8666571dc
Christian Asogwa <chrissolo2000@gmail.com>
2023-12-05 22:03:56 -0400

    signup page

--- App.js
+++ App.js
@@ -1,1 +1,19 @@
-console.log("hey git");
+function handleSubmit(event) {
+    event.preventDefault();
+
+    // Form validation (you can add more checks as needed)
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    if (!username || !email || !password) {
+        alert('Please fill in all fields');
+        return;
+    }
+
+    // Simulate successful sign-up
+    alert('Sign-up successful! Redirecting to the landing page.');
+    
+    // Redirect to the landing page (replace 'landing.html' with your actual landing page)
+    window.location.href = './login.html';
+}
